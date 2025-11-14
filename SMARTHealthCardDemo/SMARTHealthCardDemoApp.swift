82a25e52a629b7b4936951025dc8cd0c42d4f56f
Dave Carlson <dave@mtnlotus.com>
2025-11-14 15:25:49 -0500

    Initial Commit

---
+++ SMARTHealthCardDemo/SMARTHealthCardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMARTHealthCardDemoApp.swift
+//  SMARTHealthCardDemo
+//
+//  Created by David Carlson on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SMARTHealthCardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SMARTHealthCardDemoTests/SMARTHealthCardDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SMARTHealthCardDemoTests.swift
+//  SMARTHealthCardDemoTests
+//
+//  Created by David Carlson on 11/14/25.
+//
+
+import Testing
+@testable import SMARTHealthCardDemo
+
+struct SMARTHealthCardDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
